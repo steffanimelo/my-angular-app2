1d14ec3b37a23e33c755bbb5fe398fe0ddac3e28
Steffani Melo <steffani.melo@patecco.com>
2022-10-20 17:19:28 +0200

    Setting up server(s) components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // selector: '[app-servers]',   // Using app-servers as an attribute for CSS
+  // selector: '.app-servers',    //  Using app-servers as a class for CSS 
+  selector: 'app-servers',     
+  template: 
+  `<app-server></app-server>
+  <app-server></app-server>`, //templates are mandatory for every single component
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
