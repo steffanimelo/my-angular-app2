6f407552a03cf2ea6e57815b908ea12f1c3169cb
Steffani Melo <steffani.melo@patecco.com>
2022-10-25 16:15:24 +0200

    Adding Success Alert and Warning Alert components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,9 +4,10 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',   // Using app-servers as an attribute for CSS
   // selector: '.app-servers',    //  Using app-servers as a class for CSS 
   selector: 'app-servers',     
-  template: 
-  `<app-server></app-server>
-  <app-server></app-server>`, //templates are mandatory for every single component
+  // template: 
+  // `<app-server></app-server>
+  // <app-server></app-server>`, //templates are mandatory for every single component
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
