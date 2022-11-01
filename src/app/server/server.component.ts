4e40baf51f3880065b7cd698377e660474d4013d
Steffani Melo <steffani.melo@patecco.com>
2022-11-01 13:26:48 +0100

    Adding server databinding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  serverId: number = 10;  
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 
   constructor() { }
 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!Name is '+ this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
